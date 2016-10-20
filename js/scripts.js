c6755ad08cafb6de8465da8db183a28124309da1
Alex Francois and Erica Nafziger <erica@>
2016-10-20 12:00:26 -0700

    add alert for empty note field

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,32 @@ $(document).ready(function(){
     var bodytypeSeekingInput = $("#bodytypeSeeking").val();
     var petInput = $("input:radio[namej=pet]:checked").val();
     var hobbiesInput = $("input:radio[namej=hobbies]:checked").val();
+    var noteInput = $("#note").val();
 
-    if (genderInput === "woman" && genderSeekingInput === "man" && personalityInput === "extrovert" && bodytypeSeekingInput === "husky") {
-      $("#johng").show();
-    } else if (genderInput === "man" && genderSeekingInput === "woman" && personalityInput === "extrovert" && bodytypeSeekingInput === "athletic"){
-      $("#serena").show();
+    if (noteInput) {
+
+      if (genderInput === "woman" && genderSeekingInput === "man" && personalityInput === "extrovert" && bodytypeSeekingInput === "husky") {
+        $("#johng").show();
+        $("#blanks").hide();
+      } else if (genderInput === "man" && genderSeekingInput === "woman" && personalityInput === "extrovert" && bodytypeSeekingInput === "athletic"){
+        $("#serena").show();
+        $("#blanks").hide();
+      } else {
+        $("#carrot-top").show();
+        $("#blanks").hide();
+      }
     } else {
-      $("#carrot-top").show();
+      alert("Please enter a note");
     }
-
-
     event.preventDefault();
+  });
+  $("#johng button").click(function(){
+    location.reload();
+  });
+  $("#carrot-top button").click(function(){
+    location.reload();
+  });
+  $("#serena button").click(function(){
+    location.reload();
   });
 });
