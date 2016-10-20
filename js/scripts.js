90024d55b25aae60d8562d77d6b1fe6109c742cc
Alex Francois and Erica Nafziger <alex@>
2016-10-20 10:43:04 -0700

    jquery functionality allows user to choose John Goodman

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+
 $(document).ready(function(){
-  var genderInput = $("#gender").val();
-  var genderSeekingInput = $("#genderSeeking").val();
-  var personalityInput = $("#personality").val();
-  var bodytypeInput = $("#bodytype").val();
-  var bodytypeSeekingInput = $("#bodytypeSeeking").val();
-  var petInput = $("input:radio[namej=pet]:checked").val();
-  var hobbiesInput = $("input:radio[namej=hobbies]:checked").val();
+
+  $("form#blanks").submit(function(event) {
+
+
+    var genderInput = $("#gender").val();
+    var genderSeekingInput = $("#genderSeeking").val();
+    var personalityInput = $("#personality").val();
+    var bodytypeInput = $("#bodytype").val();
+    var bodytypeSeekingInput = $("#bodytypeSeeking").val();
+    var petInput = $("input:radio[namej=pet]:checked").val();
+    var hobbiesInput = $("input:radio[namej=hobbies]:checked").val();
+
+    if (genderInput === "woman" && genderSeekingInput === "man" && personalityInput === "extrovert" && bodytypeSeekingInput === "husky") {
+      $("#johng").show();
+    }
+
+    event.preventDefault();
+  });
 });
