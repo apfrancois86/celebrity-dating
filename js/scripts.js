7b936356f5f0a87bd451df05339e4f8537882c2f
Alex Francois and Erica Nafziger <alex@>
2016-10-20 09:58:16 -0700

    add variables to jQuery

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var genderInput = $("#gender").val();
+  var genderSeekingInput = $("#genderSeeking").val();
+  var personalityInput = $("#personality").val();
+  var bodytypeInput = $("#bodytype").val();
+  var bodytypeSeekingInput = $("#bodytypeSeeking").val();
+  var petInput = $("input:radio[namej=pet]:checked").val();
+  var hobbiesInput = $("input:radio[namej=hobbies]:checked").val();
+});
