9090c1fbe75f68b24538dbd3fb86bacae4a7c744
Alex Francois <apfrancois86@gmail.com>
2016-10-20 19:39:03 -0700

    add more functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,21 @@ $(document).ready(function(){
       } else if (genderInput === "man" && genderSeekingInput === "woman" && personalityInput === "introvert" && petInput === "dog" && hobbiesInput === "arts") {
         $("#amanda").show();
         $("#blanks").hide();
+      } else if (genderInput === "man" && genderSeekingInput === "woman" && petInput === "cat") {
+        $("#scarlet").show();
+        $("#blanks").hide();
+      } else if (genderInput === "man" && genderSeekingInput === "woman" && petInput === "no") {
+        $("#ryan").show();
+        $("#blanks").hide();
+      } else if (genderInput === "woman" && genderSeekingInput === "man" && bodytypeSeekingInput === "athletic") {
+        $("#channing").show();
+        $("#blanks").hide();
+      } else if (genderInput === "man" && genderSeekingInput === "man") {
+        $("#elton").show();
+        $("#blanks").hide();
+      } else if (genderInput === "woman" && genderSeekingInput === "woman") {
+        $("#ellen").show();
+        $("#blanks").hide();
       } else {
         $("#carrot-top").show();
         $("#blanks").hide();
@@ -44,6 +59,21 @@ $(document).ready(function(){
     location.reload();
   });
   $("#amanda button").click(function(){
+    location.reload();
+  });
+  $("#ryan button").click(function(){
+    location.reload();
+  });
+  $("#elton button").click(function(){
+    location.reload();
+  });
+  $("#channing button").click(function(){
+    location.reload();
+  });
+  $("#ellen button").click(function(){
+    location.reload();
+  });
+  $("#scarlet button").click(function(){
     location.reload();
   });
 });
